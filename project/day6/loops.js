05d71132b04e3edbb7803cf2aa091a5c84726973
rishikeshwagh17 <rishikesh.wagh1708@gmail.com>
2023-11-27 16:31:08 +0530

    all loops in javascript

--- project/day6/loops.js
+++ project/day6/loops.js
@@ -58,3 +58,76 @@ console.log("guess value ", guess);
 //make sure in case of while loop util you make update to variable which make while loop
 //condition false you will get infinite while loop
 
+//for of loops in js
+//nice and easy way to iterate over an array
+
+const myStudents = ["rishi", "colt", "stephen", "yuri", "soap"];
+
+//we will print element in array
+for (const student of myStudents) {
+  console.log(student);
+}
+
+//magic square is 2d array in which each array elements sum is equal
+const magicSquare = [
+  [2, 7, 6],
+  [9, 5, 1],
+  [4, 3, 8],
+];
+
+// how to iterate throught this array and find sum of all elements in each row of array
+
+for (const row of magicSquare) {
+  let sum = 0;
+  for (const el of row) {
+    sum += el;
+  }
+  console.log(` ${row} sums to ${sum}`);
+}
+
+//looping over objects
+//objects is not iterable like the arrays and strings because it has keys and values
+
+const movieReviews = {
+  Alien: 9.0,
+  Astra: 9.2,
+  Amelie: 9.4,
+  Excortist: 9.7,
+  "End game": 9.8,
+};
+
+/*try this this will not work
+for(let el of movieReviews){
+    console.log(el);
+}
+*/
+
+//so we have Objects.keys() and Objects.values() methods
+
+for (let key of Object.keys(movieReviews)) {
+  console.log(key);
+}
+
+for (let value of Object.values(movieReviews)) {
+  console.log(value);
+}
+
+//together both keys and values it will be like
+for (let movie of Object.keys(movieReviews)) {
+  console.log(movie, movieReviews[movie]);
+}
+
+//lastly for in loop
+//used for loop over keys in object
+
+const myObj = {
+  name: "rishi",
+  age: 23,
+  work: "devloper",
+  education: "Graduate",
+};
+
+for (let key in myObj) {
+  //   console.log(key);
+  console.log(key, myObj[key]);
+}
